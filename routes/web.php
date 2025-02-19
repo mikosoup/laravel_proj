ad2295e39f4e24074ce24e4a134e6d96c0fc472f
miko <117202606+mikosoup@users.noreply.github.com>
2025-02-19 21:32:08 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/students-list', [StudentsController::class, 'index']);
