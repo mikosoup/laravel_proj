c1bf2c7c91fb38f259ce82aa9b690789e9ecf472
miko <117202606+mikosoup@users.noreply.github.com>
2025-02-19 21:26:13 +0800

    Create StudentController.php

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Students;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class StudentsController extends Controller
+{
+    public function index()
+    {
+        $students = Students::all();
+        return view('studentList', compact('students'));
+    }
+}
